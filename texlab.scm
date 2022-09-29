e8a9ad2627d7710586a3c51047024f59024df8b5
Duncan Wilkie <antigravityd@gmail.com>
2022-09-29 13:47:36 -0500

    test quasiquote

--- texlab.scm
+++ texlab.scm
@@ -1885,7 +1885,8 @@ Argument Parser")
           (base32 "0aaxj9j3c40xphynyvsikpphh4bvlycw1mzgbh1rsjrmfw1iffks"))))
     (build-system cargo-build-system)
     (arguments
-     `(#:cargo-inputs
+     `(#:rust ,rust@1.61.0
+       #:cargo-inputs
         (("rust-anyhow" ,rust-anyhow-1)
          ("rust-byteorder" ,rust-byteorder-1)
          ("rust-chrono" ,rust-chrono-0.4)
